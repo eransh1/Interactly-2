eebb0125f5e72683d28707c23336174abc195a4d
eransh1 <eranshbansal@gmail.com>
2023-02-06 10:27:03 +0530

    update copying gif code

--- src/components/Video Player/VideoPlayer.js
+++ src/components/Video Player/VideoPlayer.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from "./VideoPlayer.module.css"
 import {BsCardImage} from 'react-icons/bs'
 import {AiFillCloseCircle} from "react-icons/ai"
@@ -7,15 +7,13 @@ import 'react-toastify/dist/ReactToastify.css';
 
 
 
+
 const VideoPlayer = ({data,setData}) => {
 
+//OLD WAY OF COPYING
 const copyLink=async (data)=>{
-
-
 const blob= await writeToCanvas(data.image)
-
 const blob2=new Blob([data.link],{type:'text/plain'})
-
 
 try {
     await navigator.clipboard.write([new ClipboardItem({[blob.type]:blob,[blob2.type]:blob2})])
@@ -47,7 +45,45 @@ function writeToCanvas(src){
    })
 }
 
+//BETTER CODE FOR COPYING
+async function copyGifToClipboard(gifURL, link) {
+    try {
+      // fetching GIF from the url
+      const response = await fetch(gifURL);
 
+      // converting GIF to blob
+      const blob = await response.blob();
+
+      // reader to convert the blob to dataURL
+      const reader = new FileReader();
+      reader.onloadend = async () => {
+        // using clipboard API to copy GIF to clipboard
+        await navigator.clipboard.write([
+          new ClipboardItem({
+            /**
+             * create an image element using the dataURL of GIF and paragraph element using the link
+             * then create blob of both of the string
+             * */
+
+            "text/html": new Blob(
+              [
+                `<img src='${reader.result}'><br>`,
+                `<a href='${link}'>Watch Video</a>`,
+              ],
+              { type: "text/html" }
+            ),
+          }),
+        ]);
+      };
+
+      // reading the blob as dataURL
+      reader.readAsDataURL(blob);
+
+      toast.success("GIF copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy GIF to clipboard: ", err);
+    }
+  }
 
 
   return (
@@ -59,7 +95,8 @@ function writeToCanvas(src){
                 <div className={styles.imageCont}>
                     <img className={styles.image} src={data.image} alt="gif" />   
                 </div>
-                <button onClick={()=>copyLink(data)} className={styles.CopyLinkBtn}> <span className={styles.gifIcon}><BsCardImage/></span>Copy Linked GIF</button>
+                <button onClick={()=> copyGifToClipboard(data.image,data.link)} className={styles.CopyLinkBtn}> <span className={styles.gifIcon}><BsCardImage/></span>Copy Linked GIF</button>
+                
             </div>
         </section>
     </>
